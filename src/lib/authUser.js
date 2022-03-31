4805747d8503a83b289e22896541b7744ece138e
NattReyes <psic.nataliareyes@gmail.com>
2022-03-31 13:59:11 -0600

    Se cambia la configuracion de firebase al archivo de authUser

--- src/lib/authUser.js
+++ src/lib/authUser.js
@@ -1,15 +1,28 @@
 // eslint-disable-next-line import/no-unresolved
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js';
-import { app } from './index.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js';
+// eslint-disable-next-line import/no-unresolved
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js';
 // eslint-disable-next-line import/no-cycle
 import { onNavigate } from '../main.js';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCfOwCLEL8J_PhN7mbExoCHRdkhG7J2XwU',
+  authDomain: 'probando-ando-6ad59.firebaseapp.com',
+  projectId: 'probando-ando-6ad59',
+  storageBucket: 'probando-ando-6ad59.appspot.com',
+  messagingSenderId: '1044841164809',
+  appId: '1:1044841164809:web:e2b54ac489c44747e488d8',
+  measurementId: 'G-L85FK2VG4E',
+};
+
+const app = initializeApp(firebaseConfig);
 
 // traernos inizialice
 // importar nuestros elementos de firebase igual que inicielize
 // crear una funcion llamada createUserwithEmain() espera 2 paramentros email y pass
 // la vamos a exportar y se va a ejecuar en ele compoenente correspondiente
 // Init firebase app
-export const auth = getAuth(app);
+const auth = getAuth(app);
 export function createUser(email, password) {
   createUserWithEmailAndPassword(auth, email, password) // Crea el usuario
     .then((userCredential) => {
@@ -19,9 +32,9 @@ export function createUser(email, password) {
       alert('Registrado');
       onNavigate('/UserLogin');
     })
+
     .catch((error) => {
       const errorCode = error.code;
-      // const errorMessage = error.message;
       if (errorCode === 'auth/invalid-email') {
         alert('Por favor ingresa un correo válido');
       }
