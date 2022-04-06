4eb917138301f8cdbdc6f846d2d1cdf98b395514
Dariana Quintero <darianaqh@MacBook-Air-de-Dariana.local>
2022-04-05 21:55:57 -0500

    Se agrega CSS en las dos primeras pantallas

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,21 +8,23 @@ export const Home = () => {
 
     <div class="startPage">
         <div class="headParagraphDiv">
+            <br><br><br>
             <p class="headParagraph">
-            Inicia sesión
+            Inicia sesión 
             </p>
         </div>
+        <div class="glass">
+            <div class="existingAccountButtons">
+                <button class="mainButton" id="buttonExistingUser">Iniciar con usuario</button>
+                <button class="secondButton"  id="buttonExistingGoogle">Iniciar con Google<img src="Gmail.png" align="left" id="googleimg">
+            </div>
 
-        <div class="existingAccountButtons">
-            <button class="mainButton" id="buttonExistingUser">Iniciar con usuario</button>
-            <button class="secondButton" id="buttonExistingGoogle">Iniciar con Google</button>
-        </div>
-
-        <div class="diffStep">
-            <p class="bottomParagraph">
-            ¿Aún no tienes cuenta?
-            </p>
+            <div class="diffStep">
+                <p class="bottomParagraph">
+                ¿Aún no tienes cuenta?
+                </p>
             <button class="diffStepButton" id="registerButton">Regístrate</button>
+            </div>
         </div>
     </div>
 
@@ -33,32 +35,32 @@ export const Home = () => {
 
     // Botón para iniciar con usuario existente
     const btnExistingUser = divHome.querySelector("#buttonExistingUser");
-        btnExistingUser.addEventListener("click", () => onNavigate("/userLogin"));
+    btnExistingUser.addEventListener("click", () => onNavigate("/userLogin"));
 
     // Botón para iniciar con cuenta de Google vinculada
     const btnExistingGoogle = divHome.querySelector("#buttonExistingGoogle");
-        btnExistingGoogle.addEventListener("click", () => onNavigate("/login"));
+    btnExistingGoogle.addEventListener("click", () => onNavigate("/login"));
 
     // Botón para registrar nuevo usuario
     const btnRegister = divHome.querySelector("#registerButton");
-        btnRegister.addEventListener("click", () => onNavigate("/register"));
+    btnRegister.addEventListener("click", () => onNavigate("/register"));
 
 
     return divHome;
 
 
-//   const HomeDiv = document.createElement('div');
-//   const nodoH1 = document.createElement('h1');
-//   const buttonExistingUser = document.createElement('button');
-//   const buttonExistingGoogle = document.createElement('button');
+    //   const HomeDiv = document.createElement('div');
+    //   const nodoH1 = document.createElement('h1');
+    //   const buttonExistingUser = document.createElement('button');
+    //   const buttonExistingGoogle = document.createElement('button');
 
-//   buttonExistingUser.textContent = "Iniciar con usuario";
-// //   buttonRegister.addEventListener('click', () => {
-// //     onNavigate('/register');
-// //   });
-//   buttonExistingGoogle.textContent = "Iniciar con Google";
+    //   buttonExistingUser.textContent = "Iniciar con usuario";
+    // //   buttonRegister.addEventListener('click', () => {
+    // //     onNavigate('/register');
+    // //   });
+    //   buttonExistingGoogle.textContent = "Iniciar con Google";
 
-//   nodoH1.textContent = 'Welcome my social network';
-//   HomeDiv.append(nodoH1, buttonExistingUser, buttonExistingGoogle);
-//   return HomeDiv;
+    //   nodoH1.textContent = 'Welcome my social network';
+    //   HomeDiv.append(nodoH1, buttonExistingUser, buttonExistingGoogle);
+    //   return HomeDiv;
 };
--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -2,47 +2,45 @@
 // import { onNavigate } from '../main.js';
 
 export const UserLogin = () => {
-  const htmlUserLogin = `
-        <div class="userLoginPage">
-            <div class="headParagraphDiv">
-                <p class="headParagraph">
-                Inicia sesión
-                </p>
-            </div>
+    const htmlUserLogin = `
+  <div class="userLoginPage">
+  <div class="headParagraphDiv">
+      <p class="headParagraph">
+      Inicia sesión
+      </p>
+  </div>
 
-            <div class="userAccount">
-                <form  class="loginForm" id="userLoginForm">
-                    <label for="email">Correo electrónico</label>
-                    <input type="email" id="login-email" placeholder='usuario@correo.com'/>
-                    <label for="password">Contraseña</label>
-                    <div class = "show-passwords">
-                    <input type="password" id="login-password" placeholder="******"/>
-                    <span class = "eyes-login">
-                    <i id="show3" class="fas fa-eye" style="color: #0e6359;"></i>
-                    <i id="hide3" class="fas fa-eye-slash" style="color: #0e6359;"></i>
-                    </span>
-                    </div>
-                    <br>
-                    <p id="error-message"></p>
-                    <button id="form-button-login" class="submit-btn">Ingresar</button>
-                </form>
-            </div>
+  <div class="userAccount">
+      <form  class="loginForm" id="userLoginForm">
+          <label for="email">Correo electrónico</label>
+          <input type="email" id="login-email" placeholder='usuario@correo.com'/>
+          <label for="password">Contraseña</label>
+          <input type="password" id="login-password" placeholder="******"/>
+          <span class = "eyes-login"><br>
+          <i id="show3" class="fas fa-eye" style="color: #0e6359;"></i>
+          <i id="hide3" class="fas fa-eye-slash" style="color: #0e6359;"></i>
+          </span>
+          <br>
+          <p id="error-message"></p>
+          <button id="form-button-login" class="mainButton">Ingresar</button>
+      </form>
+  </div>
 
-            <div class="diffStep">
-                <p class="bottomParagraph">
-                ¿Aún no tienes cuenta?
-                </p>
-                <button class="diffStepButton" id="registerButton">Regístrate</button>
-            </div>
-        </div>
+  <div class="diffStep">
+      <p class="bottomParagraph">
+      ¿Aún no tienes cuenta?
+      </p>
+      <button class="secondButton" id="registerButton">Regístrate</button>
+  </div>
+</div>
 
-    `;
+`;
 
-  const divUserLogin = document.createElement("div");
-  divUserLogin.innerHTML = htmlUserLogin;
+    const divUserLogin = document.createElement("div");
+    divUserLogin.innerHTML = htmlUserLogin;
 
-  const btnRegister = divUserLogin.querySelector("#registerButton");
-  btnRegister.addEventListener("click", () => onNavigate("/register"));
+    const btnRegister = divUserLogin.querySelector("#registerButton");
+    btnRegister.addEventListener("click", () => onNavigate("/register"));
 
-  return divUserLogin;
+    return divUserLogin;
 };
