4a57816baaff34e2983ba767e9576243916f3158
NattReyes <psic.nataliareyes@gmail.com>
2022-04-06 23:20:01 -0500

    se agrega la funcionalidad para agregar post en index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,26 @@
-// aqui exportaras las funciones que necesites
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+// eslint-disable-next-line import/no-unresolved
+} from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js';
+// eslint-disable-next-line import/no-cycle
+import { app } from './authUser.js';
+
+const db = getFirestore(app);
+
+export const savePost = async (inputPost) => {
+  const docRef = await addDoc(collection(db, 'post'), {
+    post: inputPost,
+  });
+  console.log('Document written with ID: ', docRef.id);
+};
+
+export const readData = async () => {
+  const querySnapshot = await getDocs(collection(db, 'post'));
+  querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, ' => ', doc.data());
+  });
+};
